d31dfc99cbba1588cfed9bf883b871f9c7687c2c
Sedeer el-Showk <s.elshowk@netherrealm.net>
2020-09-13 10:32:50 +0300

    Expanded vocac contractions (and minor cleanup)

--- __init__.py
+++ __init__.py
@@ -1,20 +1,3 @@
-# Copyright 2016 Mycroft AI, Inc.
-#
-# This file is part of Mycroft Core.
-#
-# Mycroft Core is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Mycroft Core is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
-
 from adapt.intent import IntentBuilder
 from mycroft import MycroftSkill, intent_handler
 
@@ -34,9 +17,8 @@ class WelcomeHomeSkill(MycroftSkill):
         settings will be available."""
         user_name = self.settings.get('user_name')
 
-    @intent_handler(IntentBuilder('WelcomeHome')
-                    .require('WelcomeHomeKeyword'))
-    def handle_hello_world_intent(self, message):
+    @intent_handler(IntentBuilder('WelcomeHomeIntent').require('WelcomeHomeKeyword'))
+    def handle_welcome_home_intent(self, message):
         """ Skills can log useful information. These will appear in the CLI and
         the skills.log file."""
         self.log.info("The user is home!")
